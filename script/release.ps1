36348f112f62de729bf6a653722a7832e5a27c67
P.T.Liu <meow.cs07@nctu.edu.tw>
2021-09-28 14:15:06 +0800

    Add windows release script

---
+++ script/release.ps1
@@ -0,0 +1,23 @@
+tar -cf HW1.tar `
+extern/glad `
+extern/glfw/CMake `
+extern/glfw/deps/mingw `
+extern/glfw/deps/vs2008 `
+extern/glfw/include `
+extern/glfw/src `
+extern/glfw/LICENSE.md `
+extern/glfw/CMakeLists.txt `
+extern/glm/cmake `
+extern/glm/glm `
+extern/glm/CMakeLists.txt `
+extern/glm/copying.txt `
+extern/wuffs `
+cmake `
+vs2019/HW1.sln `
+vs2019/HW1 `
+vs2019/glfw `
+include `
+src `
+.clang-format `
+CMakeLists.txt `
+README.md
